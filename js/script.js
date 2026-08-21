3887803b13e714bbf0ddeb613304b2b7851fb921
Akshata Dhumak <akshata@Akshatas-MacBook-Air.local>
2026-08-21 22:32:16 +0530

    Update mobile layouts and responsive grids

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+if(menuToggle){
+
+    menuToggle.addEventListener("click",()=>{
+
+        navLinks.classList.toggle("active");
+
+    });
+
+}
